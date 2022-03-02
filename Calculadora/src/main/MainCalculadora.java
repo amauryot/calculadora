f826c2ce4214d8e9b667b73079daae2ba20c1179
Amaury Tavares <amauryot@gmail.com>
2022-03-02 10:05:21 -0300

    v0.0.1 - Criação do app.

---
+++ Calculadora/src/main/MainCalculadora.java
@@ -0,0 +1,12 @@
+package main;
+
+import controller.Controller;
+
+public class MainCalculadora {
+
+	public static void main(String[] args) {
+		
+		Controller controller = new Controller();
+		controller.run();
+	}
+}
